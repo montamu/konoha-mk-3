311c0a6bc0b0566175930c6e7b32921b302eb026
montamu <kimremon0611@gmail.com>
2025-02-19 21:40:09 +0900

    戦績の保存と取得のクエリを作成

--- src/db/query.ts
+++ src/db/query.ts
@@ -5,21 +5,78 @@ import { game, playerResult } from './schema';
 
 // https://orm.drizzle.team/docs/connect-cloudflare-d1
 
-export const incrementWinCount = async (env: Env, gameId: number, discordGuildId: string, discordUserId: string) => {
+// playerResult table query.
+
+export const incrementWinCount = async ({
+  env,
+  gameId,
+  discordGuildId,
+  discordUserId,
+  discordDisplayName,
+} : {
+  env: Env,
+  gameId: number,
+  discordGuildId: string,
+  discordUserId: string,
+  discordDisplayName: string,
+}) => {
   const db = drizzle(env.Bindings.DB);
   return await db
-    .update(playerResult)
-    .set({ wins: sql`${playerResult.wins} + 1` })
-    .where(and(eq(playerResult.gameId, gameId), eq(playerResult.discordGuildId, discordGuildId), eq(playerResult.discordUserId, discordUserId)));
+    .insert(playerResult)
+    .values({ gameId, discordGuildId, discordUserId, discordDisplayName, wins: 1, losses: 0, resultHistory: 'W' })
+    .onConflictDoUpdate({
+      target: [playerResult.gameId, playerResult.discordGuildId, playerResult.discordUserId],
+      set: { wins: sql`${playerResult.wins} + 1`, resultHistory: sql`'W' || ${playerResult.resultHistory}` },
+    });
 };
 
-export const incrementLossCount = async (env: Env, gameId: number, discordGuildId: string, discordUserId: string) => {
+export const incrementLossCount = async ({
+  env,
+  gameId,
+  discordGuildId,
+  discordUserId,
+  discordDisplayName,
+} : {
+  env: Env,
+  gameId: number,
+  discordGuildId: string,
+  discordUserId: string,
+  discordDisplayName: string,
+}) => {
   const db = drizzle(env.Bindings.DB);
   return await db
-    .update(playerResult)
-    .set({ losses: sql`${playerResult.losses} + 1` })
-    .where(and(eq(playerResult.gameId, gameId), eq(playerResult.discordGuildId, discordGuildId), eq(playerResult.discordUserId, discordUserId)));
+    .insert(playerResult)
+    .values({ gameId, discordGuildId, discordUserId, discordDisplayName, wins: 0, losses: 1, resultHistory: 'L' })
+    .onConflictDoUpdate({
+      target: [playerResult.gameId, playerResult.discordGuildId, playerResult.discordUserId],
+      set: { losses: sql`${playerResult.losses} + 1`, resultHistory: sql`'L' || ${playerResult.resultHistory}` },
+    });
 };
+
+export const getPlayerResult = async ({
+  env,
+  gameId,
+  discordGuildId,
+  discordUserId,
+} : {
+  env: Env,
+  gameId: number,
+  discordGuildId: string,
+  discordUserId: string,
+}) => {
+  const db = drizzle(env.Bindings.DB);
+  return await db
+    .select()
+    .from(playerResult)
+    .where(
+      and(
+        eq(playerResult.gameId, gameId),
+        eq(playerResult.discordGuildId, discordGuildId),
+        eq(playerResult.discordUserId, discordUserId),
+    ));
+};
+
+// game table query.
 
 export const getActiveGames = async (env: Env) => {
   const db = drizzle(env.Bindings.DB);
@@ -27,4 +84,4 @@ export const getActiveGames = async (env: Env) => {
     .select()
     .from(game)
     .where(eq(game.active, true));
-}
+};
